68f16e425db26bc092625c58d48323e8c349107d
Maxime <maxime.gouillardon@gmail.com>
2025-02-13 19:52:37 +0100

    feat: add OrderProduct model

---
+++ app/models/order_product.ts
@@ -0,0 +1,34 @@
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import { DateTime } from 'luxon'
+import Order from '#models/order'
+import Product from '#models/product'
+
+export default class OrderProduct extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare orderId: number
+
+  @column()
+  declare productId: number
+
+  @column()
+  declare quantity: number
+
+  @column()
+  declare priceAtTime: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @belongsTo(() => Order)
+  declare order: BelongsTo<typeof Order>
+
+  @belongsTo(() => Product)
+  declare product: BelongsTo<typeof Product>
+}
