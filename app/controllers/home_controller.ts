013234878792c75032aead5e48a819c9261f7674
Maxime <maxime.gouillardon@gmail.com>
2025-02-13 15:35:35 +0100

    feat: create & implement home controller

---
+++ app/controllers/home_controller.ts
@@ -0,0 +1,11 @@
+import Product from '#models/product'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class HomeController {
+  async handle({ inertia }: HttpContext) {
+    const products = Product.all()
+    return inertia.render('home', {
+      products,
+    })
+  }
+}
