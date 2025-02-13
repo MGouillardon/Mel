920a7b0ca89fc604524099ac29b3f29117e25cf8
Maxime <maxime.gouillardon@gmail.com>
2025-02-13 19:52:12 +0100

    feat: create & implement order products table migration

---
+++ database/migrations/1739471693155_create_order_products_table.ts
@@ -0,0 +1,27 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'order_products'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('order_id').unsigned().references('id').inTable('orders').onDelete('CASCADE')
+      table
+        .integer('product_id')
+        .unsigned()
+        .references('id')
+        .inTable('products')
+        .onDelete('CASCADE')
+      table.integer('quantity').notNullable()
+      table.decimal('price_at_time', 10, 2).notNullable()
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').notNullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
+
