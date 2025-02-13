93853756f9265b0e921770a36bcc8a40d6cfaaee
Maxime <maxime.gouillardon@gmail.com>
2025-02-13 15:56:01 +0100

    refactor: optimize product sorting to fetch top 3 most expensive

--- app/controllers/home_controller.ts
+++ app/controllers/home_controller.ts
@@ -3,7 +3,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class HomeController {
   async handle({ inertia }: HttpContext) {
-    const products = await Product.all()
+    const products = await Product.query().orderBy('price', 'desc').limit(3)
     return inertia.render('home', {
       products,
     })
