3aa928d150a5d9054ba273bdd941601b80db630c
AloisSvnt <alois.sauvonnet@live.fr>
2025-02-18 14:49:53 +0100

    feat: create components CardItem directory

---
+++ inertia/components/CardItem/CardItem.tsx
@@ -0,0 +1,34 @@
+import { Link } from '@inertiajs/react';
+import type { Product } from '#types/product';
+import CoffeeBeans from './CoffeeBeans/CoffeBeans';
+
+function CardItem( {product}: {product:Product} ){
+  return (
+    <div className="card bg-base-100 shadow-xl h-full max-w-96 md:max-w-none">
+      <figure>
+        <img
+          src={product.imageUrl}
+          alt={product.name} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-xl">{product.name}</h2>
+        <span className='text-lg italic'>{product.region}</span>
+        <CoffeeBeans value={product.roastLevel} />
+        <div className="card-actions justify-end">
+        {
+          product.flavorProfile.map((flavor) => (
+            <Link href="#" className="badge hover:badge-neutral">{flavor}</Link>
+          ))
+        }
+        </div>
+        <p>{product.description}</p>
+        <div className="card-actions justify-end items-center">
+          <p className='text-xl font-black'>{product.price} €</p>
+          <Link href='#' className="btn btn-accent">Buy Now</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardItem;
