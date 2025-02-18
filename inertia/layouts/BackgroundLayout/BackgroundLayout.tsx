e2c8ef0dd25c38f290fb88eeb3744d159e35d18e
AloisSvnt <alois.sauvonnet@live.fr>
2025-02-18 14:49:06 +0100

    style: update background-color

--- inertia/layouts/BackgroundLayout/BackgroundLayout.tsx
+++ inertia/layouts/BackgroundLayout/BackgroundLayout.tsx
@@ -2,9 +2,9 @@ function BackgroundLayout({ theme }: { theme: string }) {
   const isLight = theme === 'lemonade';
 
   return isLight ? (
-    <div className="fixed inset-0 -z-10 h-screen w-screen bg-base" />
+    <div className="fixed inset-0 -z-10 h-screen w-screen bg-base-100" />
   ) : (
-    <div className="fixed inset-0 -z-10 h-screen w-screen bg-base" />
+    <div className="fixed inset-0 -z-10 h-screen w-screen bg-base-100" />
   )
 
 }
