5eba067b9c85735df03c6dde711166c3309e7bbc
AloisSvnt <alois.sauvonnet@live.fr>
2025-02-24 10:31:55 +0100

    feat: edit dir name for the rendering page

--- app/controllers/product_controller.ts
+++ app/controllers/product_controller.ts
@@ -4,11 +4,11 @@ import type { HttpContext } from '@adonisjs/core/http'
 export default class ProductController {
   async index({ inertia, request }: HttpContext) {
     const page = request.input('page', 1)
-    const limit = 10
+    const limit = 12
 
     const products = await Product.query().orderBy('created_at', 'desc').paginate(page, limit)
 
-    return inertia.render('products/index', {
+    return inertia.render('Products/Index', {
       products,
       query: {
         page,
