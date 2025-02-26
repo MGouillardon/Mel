d465bb48997adbf07c9a0ad5e9035f724e459f52
Maxime <maxime.gouillardon@gmail.com>
2025-02-26 16:28:58 +0100

    feat(routes): add all routes related to cart

--- start/routes.ts
+++ start/routes.ts
@@ -1,17 +1,9 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
 const ProductController = () => import('#controllers/product_controller')
 const HomeController = () => import('#controllers/home_controller')
 const SessionController = () => import('#controllers/Auth/session_controller')
+const CartController = () => import('#controllers/cart_controller')
 
 router.get('/', [HomeController])
 
@@ -21,6 +13,16 @@ router
     router.get('/:id', [ProductController, 'show'])
   })
   .prefix('/products')
+
+router
+  .group(() => {
+    router.get('/', [CartController, 'index'])
+    router.post('/', [CartController, 'store'])
+    router.put('/:id', [CartController, 'update'])
+    router.delete('/:id', [CartController, 'destroy'])
+    router.delete('/', [CartController, 'clear'])
+  })
+  .prefix('/cart')
 
 router
   .group(() => {
