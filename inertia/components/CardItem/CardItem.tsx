c1db29c2746010fd83c81684c4d412414473b09d
Maxime <maxime.gouillardon@gmail.com>
2025-02-27 08:49:45 +0100

    refactor(cardItem): replace inertia default link by tuyau link to access route helper

--- inertia/components/CardItem/CardItem.tsx
+++ inertia/components/CardItem/CardItem.tsx
@@ -1,34 +1,44 @@
-import { Link } from '@inertiajs/react';
-import type { Product } from '#types/product';
-import CoffeeBeans from './CoffeeBeans/CoffeeBeans';
+import { Link } from '@tuyau/inertia/react'
+import { Link as InertiaLink } from '@inertiajs/react'
+import type { Product } from '#types/product'
+import CoffeeBeans from './CoffeeBeans/CoffeeBeans'
 
-function CardItem( {product}: {product:Product} ){
+function CardItem({ product }: { product: Product }) {
   return (
-    <Link href={`/products/${product.id}`} className="card bg-base-100 shadow-xl h-full max-w-96 md:max-w-none hover:bg-base-200 transition-all">
+    <Link
+      route="products.show"
+      params={{ id: product.id }}
+      className="card bg-base-100 shadow-xl h-full max-w-96 md:max-w-none hover:bg-base-200 transition-all"
+    >
       <figure>
-        <img
-          src={product.imageUrl}
-          alt={product.name} />
+        <img src={product.imageUrl} alt={product.name} />
       </figure>
       <div className="card-body">
         <h2 className="card-title text-xl">{product.name}</h2>
-        <span className='text-lg italic'>{product.region}</span>
+        <span className="text-lg italic">{product.region}</span>
         <CoffeeBeans value={product.roastLevel} />
         <div className="card-actions justify-end">
-        {
-          product.flavorProfile.map((flavor) => (
-            <Link href="#" className="badge hover:badge-neutral">{flavor}</Link>
-          ))
-        }
+          {product.flavorProfile.map((flavor) => (
+            <Link route="products.index" className="badge hover:badge-neutral" key={flavor}>
+              {flavor}
+            </Link>
+          ))}
         </div>
         <p>{product.description}</p>
         <div className="card-actions justify-end items-center">
-          <p className='text-xl font-black'>{product.price} €</p>
-          <Link href='#' className="btn btn-accent">Add to cart</Link>
+          <p className="text-xl font-black">{product.price} €</p>
+          <InertiaLink
+            href="/cart"
+            method="post"
+            data={{ productId: product.id }}
+            className="btn btn-accent"
+          >
+            Add to cart
+          </InertiaLink>
         </div>
       </div>
     </Link>
-  );
-};
+  )
+}
 
-export default CardItem;
+export default CardItem
