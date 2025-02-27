2e3e00827a237e0252d68c492e24f9ea7389acec
Maxime <maxime.gouillardon@gmail.com>
2025-02-27 08:48:40 +0100

    feat(tuyau): implement tuyau client in front app

--- inertia/app/app.tsx
+++ inertia/app/app.tsx
@@ -1,14 +1,16 @@
 /// <reference path="../../adonisrc.ts" />
 /// <reference path="../../config/inertia.ts" />
 
-import '../css/app.css';
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
+import '../css/app.css'
+import { createRoot } from 'react-dom/client'
+import { createInertiaApp } from '@inertiajs/react'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
+import { TuyauProvider } from '@tuyau/inertia/react'
+import { tuyau } from '~/lib/tuyau'
 
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
 
-import DefaultLayout from '~/layouts/DefaultLayout/DefaultLayout';
+import DefaultLayout from '~/layouts/DefaultLayout/DefaultLayout'
 
 createInertiaApp({
   progress: { color: '#5468FF' },
@@ -16,15 +18,21 @@ createInertiaApp({
   title: (title) => `${title} - ${appName}`,
 
   resolve: async (name: string) => {
-    const pages = import.meta.glob('../pages/**/*.tsx', { eager: true }) as Record<string, { default: any }>;
-    const page = await resolvePageComponent(`../pages/${name}.tsx`, pages) as { default: any };
-    page.default.layout = page.default.layout || DefaultLayout;
-    return page;
+    const pages = import.meta.glob('../pages/**/*.tsx', { eager: true }) as Record<
+      string,
+      { default: any }
+    >
+    const page = (await resolvePageComponent(`../pages/${name}.tsx`, pages)) as { default: any }
+    page.default.layout = page.default.layout || DefaultLayout
+    return page
   },
 
   setup({ el, App, props }) {
-    
-    createRoot(el).render(<App {...props} />);
-    
+    createRoot(el).render(
+      <TuyauProvider client={tuyau}>
+        <App {...props} />
+      </TuyauProvider>
+    )
   },
-});
+})
+
